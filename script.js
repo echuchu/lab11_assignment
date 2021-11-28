08527fb0690ee7c27041a31bfe62a2b9c431a405
Eva Chu <echu58@my.bcit.ca>
2021-11-27 19:48:18 -0800

    add event listener for #contactshome and add helper functions for (2)

--- script.js
+++ script.js
@@ -33,3 +33,68 @@ let contactList = [
     },
 ]
 
+
+
+// (2)
+
+const divMain = document.querySelector(".main")
+
+// Helper function to create element and set classname
+function createElement(tag, className) {
+    let element = document.createElement(tag)
+    element.className = className
+    return element
+}
+
+// Helper function to set multiple attributes
+function setMultipleAttributes(element, attributes) {
+    for (var attributeName in attributes) {
+       element.setAttribute(attributeName, attributes[attributeName])
+    }
+ }
+
+// Removes all of the DOM nodes in div.main
+function cleanUpMain() {
+    while (divMain.firstChild) {
+        divMain.removeChild(divMain.firstChild)
+    }
+}
+
+// Creates and outputs a DOM node that is a single index/contact card
+function createSingleIndex(contact) {
+
+    let elementA = document.createElement("a")
+    elementA.setAttribute("href", "page3.html")
+
+    let elementDiv = createElement("div", "contact")
+
+    let elementP = document.createElement("p")
+    elementP.textContent = `${contact["name"]}`
+    
+    elementDiv.appendChild(elementP)
+    elementA.appendChild(elementDiv)
+    
+    return elementA
+}
+
+// Creates all of the DOM nodes that are unique to the index/Contacts page
+function renderIndex(contacts) {
+
+    for (let i = 0; i < contacts.length; i++) {
+        
+        let contact = contacts[i]
+
+        divMain.appendChild(createSingleIndex(contact))
+    }   
+}
+
+const linkContacts = document.querySelector("#contactshome")
+
+linkContacts.addEventListener("click", (e) => {
+    cleanUpMain()
+    renderIndex(contactList)
+    e.preventDefault()
+})
+
+
+
