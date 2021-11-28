b7a6f92ea57e5087c33948e043eb9077391811f7
Eva Chu <echu58@my.bcit.ca>
2021-11-27 19:25:42 -0800

    Create global contact list for (1)

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-/* This file is where all your work should go for Assignment 2.  Please remove this comment. */
+// (1)
+
+let contactList = [
+    {
+      name: "Barry Allen",
+      phone: "+1 555 555-5555",
+      address: "123 front st, Unit #1, Dakota City",
+      email: "theflash@gmail.com",
+    },
+    {
+      name: "Beverly Crusher",
+      phone: "778-555-1234",
+      address: "101 Main St, Anytown, USA",
+      email: "chiefmedicalofficer@slack.example.com",
+    }, 
+    {
+      name: "Diana Prince",
+      phone: "123-867-5309",
+      address: "Warner Brothers Animation Lot",
+      email: "wonderwoman@doc.example.com",
+    },
+    {
+      name: "Roberta Dobbs",
+      phone: "778-555-1234",
+      address: "101 Main St, Anytown, USA",
+      email: "subgenius@slack.example.com",
+    },
+    {
+      name: "Bugs Bunny",
+      phone: "123-867-5309",
+      address: "Warner Brothers Animation Lot",
+      email: "whatsup@doc.example.com",
+    },
+]
+
